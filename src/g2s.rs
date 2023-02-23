4949eba905b86306d296c629bdcb2553e753619d
alabulei1 <vivian.xiage@gmail.com>
2023-02-22 19:21:57 -0800

    Update g2s.rs

--- src/g2s.rs
+++ src/g2s.rs
@@ -4,7 +4,7 @@ use slack_flows::send_message_to_channel;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    listen_to_event("alabulei1", "github-flows", vec!["issues"], handler).await;
+    listen_to_event("alabulei1", "test", vec!["issues"], handler).await;
 }
 
 async fn handler(payload: EventPayload) {
